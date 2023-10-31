2637a5d5708a9b46dbebe8c7b91324b1edd13d9f
Yehoshua Talansky <shua@bitbean.com>
2023-10-31 15:52:54 -0400

    fixed types file

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,37 +1,39 @@
-interface ConfigOptions {
-  middleware: {
-    validator: {
-      requestValidation?: boolean;
-      responseValidation?: boolean;
-      strict?: boolean;
+export declare namespace OasTools {
+  export interface ConfigOptions {
+    middleware: {
+      validator: {
+        requestValidation?: boolean;
+        responseValidation?: boolean;
+        strict?: boolean;
+      };
+      router: {
+        disable?: boolean;
+        controllers: string;
+      };
+      error: {
+        disable?: boolean;
+        printStackTrace?: boolean;
+        customHandler?: ((...args: any[]) => void) | null; // Replace any[] with the actual argument types if possible
+      };
+      security: {
+        disable?: boolean;
+        auth?: any; // You may want to specify a more detailed type here
+      };
+      swagger: {
+        disable?: boolean;
+        path?: string;
+        ui?: any; // You may want to specify a more detailed type here
+      };
     };
-    router: {
-      disable?: boolean;
-      controllers: string;
+    oasFile: string | Record<string, any>;
+    logger: {
+      level: boolean | string;
     };
-    error: {
-      disable?: boolean;
-      printStackTrace?: boolean;
-      customHandler?: ((...args: any[]) => void) | null; // Replace any[] with the actual argument types if possible
-    };
-    security: {
-      disable?: boolean;
-      auth?: any; // You may want to specify a more detailed type here
-    };
-    swagger: {
-      disable?: boolean;
-      path?: string;
-      ui?: any; // You may want to specify a more detailed type here
-    };
-  };
-  oasFile: string;
-  logger: {
-    level: boolean | string;
-  };
-}
-export declare module "@oas-tools/core" {
-  function initialize(
+  }
+  export function initialize(
     expressApp: import("express").Express,
     initialize: ConfigOptions
   ): Promise<void>;
 }
+
+export = OasTools;
